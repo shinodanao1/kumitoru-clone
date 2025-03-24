63bbeda7f5d0f6fa14e24bbe9cabeb6ce68bbd62
shinodanao1 <shinodanao.p@gmail.com>
2025-03-24 18:48:40 +0900

    HTMLの修正

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.svgcdn.io',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
